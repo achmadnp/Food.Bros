7ab51c621a1e6f59f28cdc0cc9c5e04a439236e5
IjlalD <ijlaldwi@gmail.com>
2023-07-08 21:51:39 +0200

    input deleted

--- Food.Bros/MeineRezept.js
+++ Food.Bros/MeineRezept.js
@@ -1,11 +1,16 @@
-let isFirstEdit = true;
-
+//addRecipe hapus aje kalo udh siap
 function addRecipe() {
-  var recipeInput = document.getElementById("recipe-input");
-  var recipeName = recipeInput.value;
+  var recipeName = prompt("Enter the recipe name");
   
   if (recipeName.trim() === "") {
     alert("Please enter a recipe name");
+    return;
+  }
+  
+  var recipeType = prompt("Enter the recipe type");
+  
+  if (recipeType.trim() === "") {
+    alert("Please enter a recipe type");
     return;
   }
   
@@ -16,42 +21,58 @@ function addRecipe() {
   var nameSpan = document.createElement("span");
   nameSpan.textContent = recipeName;
   
+  var typeSpan = document.createElement("span");
+  typeSpan.textContent = recipeType;
+  typeSpan.className = "recipe-type";
+  
+  var infoContainer = document.createElement("div");
+  infoContainer.className = "recipe-info";
+  infoContainer.appendChild(nameSpan);
+  infoContainer.appendChild(typeSpan);
+  
   var editButton = document.createElement("button");
   editButton.textContent = "Bearbeiten";
+  editButton.className = "btn";
   editButton.addEventListener("click", function() {
     editRecipe(listItem);
   });
   
   var deleteButton = document.createElement("button");
   deleteButton.textContent = "Löschen";
+  deleteButton.className = "btn";
   deleteButton.addEventListener("click", function() {
     deleteRecipe(listItem);
   });
   
-  listItem.appendChild(nameSpan);
+  listItem.appendChild(infoContainer);
   listItem.appendChild(editButton);
   listItem.appendChild(deleteButton);
   
   recipeList.appendChild(listItem);
-  
-  recipeInput.value = "";
 }
 
 function editRecipe(listItem) {
-  if (isFirstEdit) {
-    window.location.href = "NeuRezept.html"; // Redirect to NeuRezept.html
-    isFirstEdit = false;
-  } else {
-    var recipeName = listItem.querySelector("span").textContent;
-    var newRecipeName = prompt("Enter the new recipe name", recipeName);
+  var nameSpan = listItem.querySelector(".recipe-info span:first-child");
+  var recipeName = nameSpan.textContent;
+  var newRecipeName = prompt("Enter the new recipe name", recipeName);
   
-    if (newRecipeName.trim() === "") {
-      alert("Please enter a recipe name");
-      return;
-    }
-  
-    listItem.querySelector("span").textContent = newRecipeName;
+  if (newRecipeName.trim() === "") {
+    alert("Please enter a recipe name");
+    return;
   }
+  
+  nameSpan.textContent = newRecipeName;
+  
+  var typeSpan = listItem.querySelector(".recipe-type");
+  var recipeType = typeSpan.textContent;
+  var newRecipeType = prompt("Enter the new recipe type", recipeType);
+  
+  if (newRecipeType.trim() === "") {
+    alert("Please enter a recipe type");
+    return;
+  }
+  
+  typeSpan.textContent = newRecipeType;
 }
 
 function deleteRecipe(listItem) {
